656f5b6307946f2bb090a0a3564d18fdb1c71dcc
justwait786178-beep <justwait786178@gmail.com>
2025-11-19 19:05:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
 ADMIN_BOT_USERNAME = "VIP_EXTRACTOR_ROBOT" #without @
 
-THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/cac7d54d83bf5871b25a7-6ff11550f49606e7ae.jpg")
+THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/6cf4e930c6bb2e2e1b68e-d5a8fb7395982d9bba.jpg")
 
 
 
@@ -47,5 +47,5 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/cac7d54d83bf5871
 # CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003427507980"))
 # MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003223932452"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/29dfb41748ec7561eac20-45780a2f354a627bda.jpg")
+# THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/6cf4e930c6bb2e2e1b68e-d5a8fb7395982d9bba.jpg")
 
