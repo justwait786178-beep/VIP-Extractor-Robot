db5a760a077188db6e63833cd24d00635b6039e9
justwait786178-beep <justwait786178@gmail.com>
2025-11-19 20:03:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ API_HASH = os.environ.get("API_HASH","61e5cc94bc5e6318643707054e54caf4")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@VIP_EXTRACTOR_ROBOT")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "VIP_EXTRACTOR_ROBOT")
 BOT_TEXT = "VIP EXTRACTOR ROBOT"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "8056915809"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003427507980"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003223932452"))
 
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003223932452")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://besib69802:YMOfgvnyjbRgW5qt@cluster0.yzzu2gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003427507980"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003223932452"))
 # -----------------------------------------------
 join = '<a href="https://t.me/Vip_Extractor_info">JOIN BACKUP😚</a>'
 # -----------------------------------------------
@@ -46,6 +46,6 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/6cf4e930c6bb2e2e
 # SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
 # CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003223932452"))
 # MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003427507980"))
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003223932452"))
 # THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/6cf4e930c6bb2e2e1b68e-d5a8fb7395982d9bba.jpg")
 
